3e7bcf4b194164f06a44f5f59d51780ee5002f2c
seifhanyyy <43908969+seifhanyyy@users.noreply.github.com>
2020-11-30 23:35:27 +0200

    Update Al-Azkar.dart
    Hoodaaaaa

--- lib/Al-Azkar.dart
+++ lib/Al-Azkar.dart
@@ -7,28 +7,35 @@ void main() {
 }
 
 class AlAzkarPage extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AlAzkarPage Page',
+      title: 'AlAzkar Page',
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(0xFFECD8A1),
-          title: Text('Al-Azkar'),
+          title: Text(
+            'Al-Azkar',
+
+            style: TextStyle(
+              color: kBackgroundColor,
+            ),
+          ),
         ),
         bottomNavigationBar: navBar,
-        body:
-        Column(
+        body:Column(
           children: <Widget>[
+            FavoriteWidget(),
             alAzkarMenu,
             Expanded(
               child: alAzkar,
             ),
           ],
         ),
+
       ),
     );
   }
+
 }
 
 final List<String> alAzkarMenuList = <String>[
@@ -121,3 +128,58 @@ Container _buildAlAzkar(String label) {
     ),
   );
 }
+
+
+
+var kBackgroundColor= Colors.black;
+var mainColor = Colors.green;
+
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  //int _favoriteCount = 5;// ···
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            //child: Text('$_favoriteCount'),
+
+          ),
+
+        ),
+      ],
+    );
+  }
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        //_favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        //_favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+}
+
+
